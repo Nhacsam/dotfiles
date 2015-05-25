28ed433324c3ab808aa199f904e6539135821614
Nicolas <nicolas@djambazian.fr>
2015-05-25 14:58:12 +0200

    Remove security softwares

--- brew.sh
+++ brew.sh
@@ -54,35 +54,9 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
+brew install exiv2
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
@@ -99,5 +73,6 @@ brew install tree
 brew install webkit2png
 brew install zopfli
 
-# Remove outdated versions from the cellar.
+
+# install outdated versions from the cellar.
 brew cleanup
